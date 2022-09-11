618a375a497f3f8961d38910eaec83febf46d61f
derfelot <derfelot.git@gmail.com>
2022-09-11 04:08:16 +0200

    poplar_kddi: init: Drop ueventd from PRODUCT_PACKAGES
    It is now a dead target, since we are directly copying it in yoshino-common.

--- device/init.mk
+++ device/init.mk
@@ -2,8 +2,7 @@
 PRODUCT_PACKAGES += \
     init.recovery.poplar_kddi \
     init.poplar_kddi \
-    fstab.poplar_kddi \
-    ueventd.poplar_kddi
+    fstab.poplar_kddi
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/config/init.poplar_kddi.nfc.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.poplar_kddi.nfc.rc
